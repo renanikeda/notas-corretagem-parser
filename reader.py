8a7c5ebd6b427c2e8f72ebd478a497b10689b470
renanikeda <renan.ikeda@hotmail.com>
2023-03-20 12:59:58 -0300

    adjust: negative notional in sell case

--- reader.py
+++ reader.py
@@ -23,7 +23,8 @@ class ParseCorretagem():
         self.end_block = end_block
         self.default_row_pattern = pp.Regex(regex_date)('data_trade') + pp.Suppress(pp.Literal('1-BOVESPA')) + pp.Word(pp.alphas)('tipo') + pp.Suppress(pp.Word(pp.alphas)) + pp.Word(pp.alphas)('nome') + pp.Suppress(pp.SkipTo(pp.White() + pp.Word(pp.nums), fail_on = '\n')) + pp.Word(pp.nums)('quantidade') + pp.Word(pp.nums + ',.')('preco').set_parse_action(parse_number) + pp.Word(pp.nums + ',.')('total').set_parse_action(parse_number)
         self.columns = ['Data Trade', 'Tipo', 'Nome', 'Quantidade', 'Preço', 'Total']
-        self.parsed_pdf = ''
+        self.parsed_pdf = None
+        self.rows_pdf = None
             
     def generate_rows(self):
         rows_pdf = ''
@@ -40,7 +41,9 @@ class ParseCorretagem():
         return rows_pdf
 
     def parse(self, pattern = None):
-        block = self.generate_rows()
+        if not self.rows_pdf:
+            self.generate_rows()
+        block = self.rows_pdf
         row_pattern = self.default_row_pattern if pattern is None else pattern
         self.parsed_pdf = row_pattern.searchString(block)
         return self.parsed_pdf
@@ -52,17 +55,22 @@ class ParseCorretagem():
         pdParsedPdf['Quantidade'] = pdParsedPdf['Quantidade'].astype(float)
         pdParsedPdf['Preço'] = pdParsedPdf['Preço'].astype(float)
         pdParsedPdf['Total'] = pdParsedPdf['Total'].astype(float)
+        pdParsedPdf.loc[pdParsedPdf['Tipo'] == 'V', 'Quantidade'] = pdParsedPdf.loc[pdParsedPdf['Tipo'] == 'V', 'Quantidade']*(-1)
         return pdParsedPdf
     
     def mean_price(self):
         pdParsedPdf = self.get_df()
         assets = pdParsedPdf['Nome'].unique()
-        mean_df = pd.DataFrame([], columns = ['Nome', 'Quantidade', 'Preço Médio'])
+        mean_df = pd.DataFrame([], columns = ['Nome', 'Quantidade', 'Preço Médio', 'Posição Final'])
         for asset in assets:
             subDf = pdParsedPdf[(pdParsedPdf['Nome'] == asset) & (pdParsedPdf['Tipo'] == 'C')]
             mean_price = subDf.groupby('Nome').apply(lambda df: round(np.average(df['Preço'], weights=df['Quantidade']), 2)).values[0]
-            notional = subDf['Quantidade'].sum(numeric_only = True)
-            mean_df.loc[len(mean_df)] = { 'Nome': asset, 'Quantidade': notional, 'Preço Médio': mean_price } 
+            notional = (subDf['Quantidade'].sum(numeric_only = True))
+
+            subDf = pdParsedPdf[pdParsedPdf['Nome'] == asset]
+            position = round((subDf['Preço']*subDf['Quantidade']).sum(), 2)
+
+            mean_df.loc[len(mean_df)] = { 'Nome': asset, 'Quantidade': notional, 'Preço Médio': mean_price, 'Posição Final': position } 
         return mean_df
 
 
